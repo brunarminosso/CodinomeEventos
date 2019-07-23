ce8c2eb44f59e8880a5e442fd4c312554d7d4a84
Aline <aline.escobar@outlook.com>
2019-07-23 16:57:57 -0300

    Código tela pesquisa

--- CodinomeEventos/ViewController/PesquisaViewController.swift
+++ CodinomeEventos/ViewController/PesquisaViewController.swift
@@ -8,51 +8,16 @@
 
 import UIKit
 
-struct Fornecedor: Codable {
-    let nome : String
-    let email: String
-    let senha: String
-    let cep: String
-    let endereco: String
-    let numero: Int
-    let bairro: String
-    let cidade: String
-    let estado: String
-    let telefone_fixo: String
-    let celular: String
+
+
+class PesquisaViewController: UIViewController{
     
-    enum CondingKeys: String, CodingKey  {
-        case nome = "nome"
-        case email = "email"
-        case senha = "senha"
-        case cep = "cep"
-        case endereco = "endereco"
-        case numero = "numero"
-        case bairro = "bairro"
-        case cidade = "cidade"
-        case estado = "estado"
-        case telefone_fixo = "telefone_fixo"
-        case celular = "celular"
-    }
-}
-
-class PesquisaViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+//        tableView.delegate = self
+//        tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
+
