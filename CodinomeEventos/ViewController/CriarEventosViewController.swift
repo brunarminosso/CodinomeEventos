7ce59e4a1db0969b7ecfce90f90aeac5c5415360
Aline <aline.escobar@outlook.com>
2019-07-29 13:00:42 -0300

    criação de novas classes view controlar

--- CodinomeEventos/ViewController/CriarEventosViewController.swift
+++ CodinomeEventos/ViewController/CriarEventosViewController.swift
@@ -33,7 +33,7 @@ class CriarEventosViewController: UIViewController {
     }
     
     @IBAction func buttonGravar(_ sender: Any) {
-    
+        
         self.ref.child("eventos/0/name").setValue(campoNomeEvento.text as? NSString)
 //        print("executou o action do botao")
     }
