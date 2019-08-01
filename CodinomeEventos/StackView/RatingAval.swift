8fbdaaf870203abe2d159ef83c6004f736157f33
Aline <aline.escobar@outlook.com>
2019-08-01 11:56:28 -0300

    Detalhes

---
+++ CodinomeEventos/StackView/RatingAval.swift
@@ -0,0 +1,76 @@
+//
+//  RatingControl.swift
+//  CodinomeEventos
+//
+//  Created by Aline Osana Escobar on 24/07/19.
+//  Copyright © 2019 Bruna Rafaela Martins Minosso. All rights reserved.
+//
+
+import UIKit
+//codigo apenas para visualizacao do avaliacao
+//falta linkar o valor da avaliacao com a armazenada no banco de dados
+@IBDesignable class RatingAval: UIStackView {
+    
+    //MARK: Properties
+    
+    @IBInspectable var starSize: CGSize = CGSize(width: 40.0, height: 40.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    private var ratingButtons = [UIButton]()
+    var rating = 0
+    
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    @objc func rattingButtonTapped (button: UIButton){
+//        print("Button pressed!")
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    //MARK: Private Methods
+    private func setupButtons () {
+        //Clear any existing buttons
+        
+        
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        let bundle = Bundle(for: type(of: self))
+        
+        let estrelavaziapequena = UIImage(named: "estrela_vazia_pq", in: bundle, compatibleWith: self.traitCollection)
+        let estrelacheiapequena = UIImage(named: "estrela_cheia_pq", in: bundle, compatibleWith: self.traitCollection)
+        //        let estrelaVazia = UIImage(named: "estrela_vazia", in: bundle, compatibleWith: self.traitCollection)
+        
+        for _ in 0..<starCount {
+            let button = UIButton()
+            
+            button.setImage(estrelavaziapequena, for: .normal)
+            button.setImage(estrelacheiapequena, for: .selected)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            //        Setup the button action
+            //        button.addTarget(self, action: #selector(RatingControl.rattingButtonTapped(button:)), for: .touchUpInside)
+            //        Add the button to the stack
+            addArrangedSubview(button)
+            //        Add the new button to the rating button array
+            
+        }
+    }
+}
--- CodinomeEventos/ViewController/DetalhesFornecedorViewController.swift
+++ CodinomeEventos/ViewController/DetalhesFornecedorViewController.swift
@@ -16,13 +16,14 @@ class DetalhesFornecedorViewController: UIViewController {
     @IBOutlet weak var detalhe_categoria: UILabel!
     
     @IBOutlet weak var evaluate_label: UILabel!
-    
-    var detalheCategoria: String = ""
-    var detalheImagemProdutos: String = ""
-    var detalheNomeFornecedor: String = ""
-    var detalheDescricao: String = ""
-    
+    @IBOutlet weak var descricao: UITextView!
+    @IBOutlet weak var nota: UILabel!
+    @IBOutlet weak var endereco: UILabel!
+    @IBOutlet weak var nome_fornecedor: UILabel!
     @IBOutlet weak var evaluate_button: UIButton!
+    
+    var fornecedor: Fornecedores?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,13 +37,21 @@ class DetalhesFornecedorViewController: UIViewController {
         moldura.layer.shadowOpacity = 0.23
 
         evaluate_label.font = UIFont(name: "Lato-semibold", size: CGFloat(20))
-    
+        
+        detalhe_categoria.text = fornecedor?.categoria
+        nome_fornecedor.text = fornecedor?.nome
+        descricao.text = fornecedor?.descricao
+        endereco.text = fornecedor?.endereco
+        nota.text = "\(fornecedor?.notaAvaliacaoMedia ?? 0)" 
+//        endereco.text = 
         
         evaluate_button.layer.cornerRadius = 15.0
         evaluate_button.tintColor = UIColor.white
         evaluate_button.backgroundColor = UIColor(red: 0.16, green: 0.67, blue: 0.89, alpha: 1)
         evaluate_button.titleLabel?.font = UIFont(name: "Lato-semibold", size: CGFloat(14))
         // Do any additional setup after loading the view.
+        
+//        detalhe_categoria.text =
     }
     
 
